767c6d98ea41474fff0b52ffe0b088e88d600064
becia.robert21@gmail.com <Vibevibe23!>
2022-06-07 21:46:29 +0300

    my first file

---
+++ MODUL II/Part II.py
@@ -0,0 +1,78 @@
+# var = "My Python3 Course"
+#
+# print(var)
+# print(var[3::2])
+#
+# print(var[3:9:2])
+#
+# print(var[-1:-5:-2])
+#
+#
+# var1=input("text aici :")
+#
+# print(var1==var1[::-1])
+
+
+# var2= "Hello Python"
+# var3 = "Ana are mere"
+# var4 = ""
+# print(var2[0:5],var2[6:],sep="_")
+# print(var2,end=".\n")
+# print(var2[0:5]*4)
+#
+# var2= "Hello Python"
+# print(var2,end=".\n")
+# print(var2[0:5]*4)
+#
+# a = float(5.)
+# b = 5
+# c = "ana"
+#
+# print("locatia lui a este:",hex(id(a)),type(a))
+
+
+
+# var5= r"Normal string \n"
+#
+# var6= 'Normal \n string'
+#
+# var7= """TEEXT
+# AICI"""
+#
+# print(var5)
+# print(var6)
+# print(var7)
+
+#
+# formatvar = f"you will have a print here - {input()}"
+#
+#
+# print(formatvar)
+
+# var8 = 2022
+#
+# input("Cum te numesti?")
+# var= int(input("Ce varsta ai?"))
+# print(f"Ceau Ana! Deci te-ai nascut in {var8-var}")
+
+
+
+# var1 = "Astazi ma duc la \"facultate\""
+# var2 = "/*\/*\/*\/*\/*\\"
+# var3 = "Python"
+# var4 = "\./\./\./\./"
+# var5 = "P\ty\tt\th\to\tn"
+#
+#
+# print(var1)
+# print(var2)
+# print(var3)
+# print(var4)
+# print(var5)
+
+
+
+
+var1 = "insert "
+var1 = var1.replace("i","_",2)
+print("Replaced i: ",var1*3)
